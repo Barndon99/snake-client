9dec36f95e02eafdf1de4a14faa50ebcb4bd4ef8
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-02 14:42:02 -0600

    added the ability for the program to see user input

--- play.js
+++ play.js
@@ -1,7 +1,23 @@
 const net = require("net");
 const connection = require("./client")
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+  };
+};
+
+setupInput();
 
 console.log("Connecting ...");
 connection.connect();
